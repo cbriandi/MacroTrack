a09addb6efdb401a967decb27e50470063378a1c
cbriandi <briandicharles@gmail.com>
2025-07-15 19:22:12 -0400

    added functional recipe controller for creating a recipe

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,5 +1,29 @@
-const createRecipe = (req, res) => {
-  res.json({ msg: 'Recipe creation endpoint works!' }); // just testing for now
+const Recipe = require('../models/Recipe');
+
+const createRecipe = async (req, res) => {
+  const { title, ingredients, instructions, macros, isPublic } = req.body;
+
+  if (!title || !ingredients || !instructions) {
+    return res.status(400).json({ msg: 'Title, ingredients, and instructions are required.' });
+  }
+
+  try {
+    const newRecipe = new Recipe({
+      title,
+      ingredients,
+      instructions,
+      macros: macros || {},
+      isPublic: isPublic || false,
+      user: req.user.userId,
+    });
+
+    const savedRecipe = await newRecipe.save();
+
+    res.status(201).json(savedRecipe);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 };
 
 module.exports = { createRecipe };
