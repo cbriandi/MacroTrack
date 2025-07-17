6abe9b9bfcf85f46429b33467da3f2ac60e9e45f
cbriandi <briandicharles@gmail.com>
2025-07-17 19:05:39 -0400

    Used flowbite template for baseline register page, added logic to handle user registration

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -6,48 +6,84 @@ export default function Register() {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`Successfully registered! Credentials are: ${email}, ${username}, ${password}.`);
+
+    if(!email || ! username || !password || !confirmPassword) {
+      alert('All fields are required to register.');
+      return;
+    }
+
+    if(password !== confirmPassword) {
+      alert('Passwords do not match.');
+      return;
+    }
+
+    console.log(`Successfully registered! Credentials are: ${email}, ${username}, ${password}, ${confirmPassword}.`);
   }
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-4 border rounded shadow">
-      <h1 className="text-2xl font-bold mb-4">Register</h1>
-
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input 
-          type="text" 
-          required 
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          />
-        <label>Username</label>
-        <input 
-          type="text" 
-          required 
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          />
-        <label>Password</label>
-        <input 
-          type="password" 
-          required 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          />
-        <button type='submit'>Register</button>
-        <p>{email}, {username}, {password}</p>
-      </form>
-
-      <p className="text-sm">
-        Already have an account?{" "}
-        <Link to="/login" className="text-blue-500 underline">
-          Login here
-        </Link>
-      </p>
-    </div>
+   <section className="bg-gray-50 dark:bg-gray-900 font-body">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
+                MacroTrack   
+            </a>
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-left">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Create an account
+                    </h1>
+                    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input 
+                              type="email"
+                              required
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                              placeholder="name@company.com" />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                            <input 
+                              type="username"
+                              value={username}
+                              onChange={(e) => setUsername(e.target.value)}
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                              placeholder="name"/>
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input 
+                              type="password" 
+                              required
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                              placeholder="••••••••" 
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                            <input 
+                              type="password" 
+                              required
+                              value={confirmPassword}
+                              onChange={(e) => setConfirmPassword(e.target.value)}
+                              placeholder="••••••••" 
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                        </div>
+                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                            Already have an account? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </section>
   );
 }
